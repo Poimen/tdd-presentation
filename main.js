f7cfd1336797fd443c324eaa4d402bd33486cde3
Richard Shephard <richard.shephard@gmail.com>
2023-03-15 18:57:46 +0200

    remove unused files

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ import 'reveal.js/dist/reset.css';
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/moon.css';
 import 'reveal.js/plugin/highlight/monokai.css';
-import './style.css'
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import HighLight from 'reveal.js/plugin/highlight/highlight.js';
 import Notes from 'reveal.js/plugin/notes/notes.js';
 import RMath from 'reveal.js/plugin/math/math.js';
-import p from './presentation.md';
+// import p from './presentation.md';
 
 let deck = new Reveal({
    plugins: [ Markdown, HighLight, Notes, RMath ]
